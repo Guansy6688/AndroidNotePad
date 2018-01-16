8dd4a229d820dbaf778234d97613fcce1ea743d6
downson <downson@downson.local>
2018-01-16 09:22:19 -0500

    First Commit

---
+++ app/src/main/java/com/csi5175/mobilecommerce/quicknote/MainActivity.java
@@ -0,0 +1,13 @@
+package com.csi5175.mobilecommerce.quicknote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
